996299e55530b4a15a589460cf6dfb01fd6cb636
mahede182 <mahede.object@gmail.com>
2026-02-26 14:20:41 +0600

    feat: added create and retrieve post implementation

---
+++ backend/src/controllers/post.controller.js
@@ -0,0 +1,38 @@
+const Post = require('../models/post.model');
+
+const createPost = async (req, res) => {
+    try {
+        const { text } = req.body;
+
+        const userId = req.user ? req.user.id : req.body.userId;
+
+        if (!text || !userId) {
+            return res.status(400).json({ error: "Text and userId are required" });
+        }
+
+        const newPost = new Post({
+            userId,
+            text
+        })
+        const savedPost = await newPost.save();
+        res.status(201).json(savedPost);
+    } catch (error) {
+        console.error("Create Post Error:", error);
+        res.status(500).json({ error: "Failed to create Post", details: error.message })
+    }
+}
+
+const getPosts = async (req, res) => {
+    try {
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 10;
+        const skip = (page - 1) * limit
+
+        const posts = await Post.find().populate('userId', 'username').sort({ createdAt: -1 }).skip(skip).limit(limit);
+        res.status(200).json(posts);
+    } catch (error) {
+        res.status(500).json({ error: "Failed to get Posts" })
+    }
+}
+
+module.exports = { createPost, getPosts };
---
+++ backend/src/models/post.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    text: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    likes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model('Post', postSchema)
